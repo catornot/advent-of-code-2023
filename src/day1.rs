254c02e0eb0c5f6e6593abad3ff46ffd16d5eb5c
cat_or_not <41955154+catornot@users.noreply.github.com>
2023-11-30 20:01:29 -0500

    add day1

---
+++ src/day1.rs
@@ -0,0 +1,21 @@
+use crate::Day;
+
+pub struct Day1;
+
+impl Day for Day1 {
+    fn example_input(&self) -> &'static str {
+        todo!()
+    }
+
+    fn example_solution(&self) -> (&'static str, &'static str) {
+        todo!()
+    }
+
+    fn part_1(&mut self, input: String) -> String {
+        todo!()
+    }
+
+    fn part_2(&mut self, input: String) -> String {
+        todo!()
+    }
+}
