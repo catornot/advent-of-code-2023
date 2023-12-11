f2c4508d7b4900fd576bb4ffb8b25fa088bb5dd0
catornot <41955154+catornot@users.noreply.github.com>
2023-12-11 16:12:28 -0500

    day 11 prep

--- src/day11.rs
+++ src/day11.rs
@@ -4,16 +4,49 @@ pub struct Day11;
 
 impl Day for Day11 {
     fn example_input(&self) -> (&'static str, &'static str) {
-        ("", "")
+        (
+            r#"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."#,
+            "",
+        )
     }
 
     fn example_solution(&self) -> (&'static str, &'static str) {
-        ("", "")
+        ("374", "")
     }
 
     fn part_1(&mut self, input: String) -> String {
-        _ = input;
-        "".to_string()
+        let mut galatic_map = input
+            .lines()
+            .map(|line| line.chars().map(|c| c == '#').collect())
+            .collect::<Vec<Vec<bool>>>();
+
+        // todo implement doubling yk
+
+        let len = galatic_map.len();
+        (0..(galatic_map[0].len()))
+            .into_iter()
+            .map(|x| (0..len).into_iter().map(move |y| [x, y]))
+            .flatten()
+            .map(|[x, y]| {
+                if !galatic_map[y][x] {
+                    return 0;
+                }
+
+                galatic_map.iter()
+
+                todo!()
+            })
+            .sum::<u64>();
+        todo!()
     }
 
     fn part_2(&mut self, input: String) -> String {
